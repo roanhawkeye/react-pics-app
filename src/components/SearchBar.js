0389bf0d46b72445b7a8b3f00651b16e1c393ec6
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-29 11:45:42 -0600

    Add a callback to call onsubmit in out App component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,8 +5,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-
-    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
