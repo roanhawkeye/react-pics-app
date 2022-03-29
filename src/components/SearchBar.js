582d516a30fa885fdf5179ba3910b197da83fd9c
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-29 11:37:36 -0600

    Add a callback to the submit method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,22 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => this.setState(e.target.value)}
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
